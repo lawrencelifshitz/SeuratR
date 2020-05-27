19b30cb9f6162187f9acd3f7881220a2c4d189fe
lawrencelifshitz <53879135+lawrencelifshitz@users.noreply.github.com>
2020-05-27 18:29:01 -0400

    Create install.R

---
+++ install.R
@@ -0,0 +1,7 @@
+install.packages("Seurat")
+install.packages("ggplot2")
+install.packages("cowplot")
+install.packages("dplyr")
+install.packages("tictoc")
+# install.packages("clusterProfiler")  # for CompareClusters2.R
+# BiocManager::install("org.Hs.eg.db")  
