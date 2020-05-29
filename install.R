5ee0823a9b419996c50368ab1134c915a9015164
lawrencelifshitz <53879135+lawrencelifshitz@users.noreply.github.com>
2020-05-29 11:26:04 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,10 @@
+# not sure if I can "install" from other locations (eg. Bioconductor), mpan is the default. try it:
+# https://github.com/jupyter/repo2docker/issues/24  - see comments near bottom.
+# Seurat needs mutoss and mutoss needs multtest.  I suppose if I DO get installs from Bioconductor working I
+# could just install Seurat from there too and it would all happen automatically. But I started by trying
+# to install everything from mpan, but then I'm left with trying to grab multteest from Bioconductor:
+install.packages("BiocManager")
+BiocManager::install("multtest")  # no longer provided at rpan (or mpan), need to get it from Bioconductor.
 install.packages("Seurat")
 install.packages("ggplot2")
 install.packages("cowplot")
